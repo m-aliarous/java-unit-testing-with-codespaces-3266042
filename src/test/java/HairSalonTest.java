c7b768b300ca592973a43533f733ebd9de1692eb
mostefa <mostefa.aliarous@gmail.com>
2024-04-20 21:08:29 +0100

    chapter 3 challenge

--- src/test/java/HairSalonTest.java
+++ src/test/java/HairSalonTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class HairSalonTest {
 
@@ -6,22 +7,29 @@ class HairSalonTest {
 
     @Test
     public void testGetOpeningDays() {
-
+        var expected  = new String[]{"Monday", "Tuesday"};
+        var actual  = hairSalon.getOpeningDays();
+        assertArrayEquals(expected, actual);
+        
     }
 
     @Test
     public void testSalonIsOpenOnMonday() {
-
+        assertTrue(hairSalon.isOpen("Monday"));
     }
 
     @Test
     public void testSalonIsClosedOnWednesday() {
-
+     assertFalse(hairSalon.isOpen("Wednesday"));
     }
 
     @Test
     public void testExceptionIsThrownIfWeekdayDoesNotEndInDay() {
-
+        Exception exception =assertThrows(IllegalArgumentException.class, () -> hairSalon.isOpen("SomeDay"));
+        String actual = exception.getMessage();
+        String expected = "only weekdays are valid";
+        assertEquals(expected, actual);
+        
     }
 
 }
