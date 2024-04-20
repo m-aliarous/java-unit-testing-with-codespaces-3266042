bf78be43b00a2ef7b3a25683fdd33c65b7ae4105
mostefa <mostefa.aliarous@gmail.com>
2024-04-20 09:51:53 +0100

    calculator test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,6 +12,33 @@ public class CalculatorTest {
         int expected = 3;
 
         int actual = calculator.add(1, 2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSubtract() {
+        int expected = 1;
+
+        int actual = calculator.subtract(2, 1);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMultiply() {
+        int expected = 2;
+
+        int actual = calculator.multiply(1, 2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testDivide() {
+        int expected = 2;
+
+        int actual = calculator.divide(4, 2);
 
         assertEquals(expected, actual);
     }
